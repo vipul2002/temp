01c617378ec456a9ea90f7c77dfe5e5e81b7313e
vipul2002 <vipulbelhekar20@gmail.com>
2024-08-31 14:30:37 +0530

    first commit

---
+++ faltu/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbarcomponent'
+function App() {
+  return (
+    <>
+      <NavbarComponent /> 
+      
+        <h1>Welcome to My Website</h1>
+        <p>This is the home page content.</p>
+      
+  
+  </>
+  );
+}
+
+export default App;
---
+++ faltu/src/components/Navbarcomponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+export default function Navbarcomponent() {
+  return (
+    <h1>hello </h1>
+   
+  );
+}
